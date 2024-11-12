69a6313d31f321e267f59872c09dd2f8b878480a
space086 <space_086@naver.com>
2024-11-12 16:37:24 +0900

    🕰️ react-study/ 241112 - day2  class 함수와 state

--- src/App.js
+++ src/App.js
@@ -1,66 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => {
+      return {
+        count: current.count + 1,
+      };
+    });
+  };
+  minus = () => {
+    this.setState((current) => {
+      return {
+        count: current.count - 1,
+      };
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add} Add></button>
+        <button onClick={this.minus} Minus></button>
+      </div>
+    );
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "bulgogi",
-    image: "https://www.foodjang.com/New/03/221006979/221006979_b_2.jpg",
-    rating: 4.3,
-  },
-  {
-    id: 3,
-    name: "kimbap",
-    image:
-      "https://www.sbfoods-worldwide.com/ko/recipes/deq4os00000007qr-img/6_Kinpa.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 4,
-    name: "samgyetang",
-    image:
-      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    rating: 4.7,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
 export default App;
