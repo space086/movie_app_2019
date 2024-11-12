d71e933b3dec1c0e6dea601e56afbc40c4cf1ad0
space086 <space_086@naver.com>
2024-11-12 16:05:39 +0900

    🕰️ react-study /21112 - day2 / propTypes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 5,
   },
   {
+    id: 2,
     name: "bulgogi",
-    image:
-      "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+    image: "https://www.foodjang.com/New/03/221006979/221006979_b_2.jpg",
+    rating: 4.3,
   },
   {
+    id: 3,
     name: "kimbap",
     image:
-      "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+      "https://www.sbfoods-worldwide.com/ko/recipes/deq4os00000007qr-img/6_Kinpa.jpg",
+    rating: 4.9,
   },
   {
+    id: 4,
     name: "samgyetang",
     image:
       "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 4.7,
   },
 ];
 
@@ -37,7 +52,12 @@ function App() {
     <div>
       <h1>Hello</h1>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
